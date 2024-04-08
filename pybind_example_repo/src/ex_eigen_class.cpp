a9b49d88753f49fbe4f7df98a25f8e829ddd172a
Trent Yarosevich <tyarosevich@github.com>
2024-04-08 15:17:51 -0700

    Overloaded constructor appears to be passing by reference as desired.

--- pybind_example_repo/src/ex_eigen_class.cpp
+++ pybind_example_repo/src/ex_eigen_class.cpp
@@ -5,8 +5,14 @@
 namespace py = pybind11;
 
 class VecWrapper {
-    Eigen::MatrixXd small_vec = Eigen::MatrixXd::Zero(10, 1);
 public:
+    Eigen::MatrixXd small_vec;
+    VecWrapper() {
+        small_vec = Eigen::MatrixXd::Zero(10, 1);
+    }
+    VecWrapper(Eigen::Ref<Eigen::MatrixXd> v){
+    small_vec = v;
+    }
     Eigen::MatrixXd &getMatrix() { return small_vec; }
     const Eigen::MatrixXd &viewMatrix() { return small_vec; }
 };
@@ -14,6 +20,7 @@ public:
 void init_ex_eigen_class(py::module_ &m) {
     py::class_<VecWrapper>(m, "VecWrapper")
         .def(py::init<>())
+        .def(py::init<Eigen::Ref<Eigen::MatrixXd>>())
         .def("copy_matrix", &VecWrapper::getMatrix) // Makes a copy!
         .def("get_matrix", &VecWrapper::getMatrix, py::return_value_policy::reference_internal)
         .def("view_matrix", &VecWrapper::viewMatrix, py::return_value_policy::reference_internal)
--- pybind_example_repo/tests/test.py
+++ pybind_example_repo/tests/test.py
@@ -1,7 +1,7 @@
 import python_example as m
 import numpy as np
 
-assert m.__version__ == '0.0.4'
+assert m.__version__ == '0.0.6'
 
 # Test functions from other source files.
 assert m.add(1, 2) == 3
@@ -16,10 +16,24 @@ p.setName("Charly")
 print(p.getName())
 
 # Test eigen matrix from other source file.
+print("Testing default initialization.")
 a = m.VecWrapper()
-m = a.get_matrix()  # flags.writeable = True,  flags.owndata = False
+mat_from_eigen = a.get_matrix()  # flags.writeable = True,  flags.owndata = False
 v = a.view_matrix()  # flags.writeable = False, flags.owndata = False
 c = a.copy_matrix()
 
-print(m)
-print(type(m))
+print(mat_from_eigen)
+print(type(mat_from_eigen))
+
+print("Testing initialization with numpy array.")
+b = np.random.rand(10,)
+print("Numpy array:")
+print(b)
+print("Eigen matrix")
+a = m.VecWrapper(b)
+mat_from_eigen = a.get_matrix()  # flags.writeable = True,  flags.owndata = False
+v = a.view_matrix()  # flags.writeable = False, flags.owndata = False
+c = a.copy_matrix()
+print(mat_from_eigen)
+print(type(mat_from_eigen))
+print(mat_from_eigen.flags.owndata)
